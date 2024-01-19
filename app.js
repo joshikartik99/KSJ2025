1ab134049496a687ece06d9a1658b5f0d6c96d13
Kartik Joshi <142908116+KSJ2025@users.noreply.github.com>
2024-01-19 19:41:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+(function() {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+
+var typed = new Typed(".multiple-text", {
+    strings: ["Web Developer", "PCB Designer", "Digital System Designer"],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelat: 1000,
+    loop: true
+
+})
